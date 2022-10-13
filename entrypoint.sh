e164cceaa4d491b45fd7ca31492bad23bb44e46b
yizhihouzi <chtsx@qq.com>
2022-10-13 18:31:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,8 @@ mkdir -p /github/home/.ssh
 
 eval $(ssh-agent -s)
 
-echo -e "StrictHostKeyChecking no" >> ~/.ssh/ssh_config
+echo 'Host *' >> ~/.ssh/config
+echo 'StrictHostKeyChecking no' >> ~/.ssh/config
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' >/tmp/id_rsa
 chmod 600 /tmp/id_rsa
 ssh-add /tmp/id_rsa
